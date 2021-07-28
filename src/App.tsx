02b5e32481f8aff711f62899992c6f724dff0c8f
avasthi-git <ishan.avasthi@outlook.com>
2021-07-28 10:21:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//Components
+import Header from "./components/header"
+import Player from "./components/Player"
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Player/>
+    </div>
+  );
+}
+
+export default App;
